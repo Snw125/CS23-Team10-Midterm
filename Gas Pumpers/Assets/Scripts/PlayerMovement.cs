1c61c04027be0bd57768d9e4576326f3c42848b2
Snw125 <123609058+Snw125@users.noreply.github.com>
2023-02-18 21:52:37 -0500

    Player Movement
    Created player instance, started player movement script

---
+++ Gas Pumpers/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw ("Horizontal");
+        movement.y = Input.GetAxisRaw ("Vertical");
+    }
+}
