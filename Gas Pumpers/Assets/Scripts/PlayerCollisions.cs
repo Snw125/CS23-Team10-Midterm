a1f5e6f74053b936f87c4cf19cd92a8476a5df98
Snw125 <123609058+Snw125@users.noreply.github.com>
2023-02-28 17:52:21 -0500

    Submitted HW5 Version

--- Gas Pumpers/Assets/Scripts/PlayerCollisions.cs
+++ Gas Pumpers/Assets/Scripts/PlayerCollisions.cs
@@ -2,26 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerCollisions : MonoBehaviour
 {
     public GameObject gameOverText;
 
     void Start() {
-            gameOverText = GameObject.FindWithTag("GameOver");
-            gameOverText.SetActive(false);
+            // gameOverText = GameObject.FindWithTag("GameOver");
+            // gameOverText.SetActive(false);
     }
     
     public void OnTriggerEnter2D(Collider2D other){
             if (other.gameObject.tag == "Car") {
                     Debug.Log("Game Over!");
-                    gameOverText.SetActive(true);
+                    //gameOverText.SetActive(true);
                     Time.timeScale = 0f;
+                    SceneManager.LoadScene("EndScene");
             }
             if (other.gameObject.tag == "Enemy") {
                     Debug.Log("Game Over!");
-                    gameOverText.SetActive(true);
+                    //gameOverText.SetActive(true);
                     Time.timeScale = 0f;
+                    SceneManager.LoadScene("EndScene");
             }
     }
 }
