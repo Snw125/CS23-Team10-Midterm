4e3872359ae6a6638823d9d5c9936dfff2917150
Walter254 <53183305+Walter254@users.noreply.github.com>
2023-03-06 17:30:23 -0500

    lives  system

---
+++ Gas Pumpers/Assets/Scripts/health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class health : MonoBehaviour
+{
+
+    public Image[] hearts; // array of heart images
+    public Sprite fullHeart; // full heart sprite
+    public Sprite emptyHeart; // empty heart sprite
+    public GameHandler gameHandler; // reference to the LivesManager script
+
+    private void Update()
+    {
+        int lives = gameHandler.playerLives; // get the lives variable from the LivesManager script
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < lives)
+            {
+                hearts[i].sprite = fullHeart; // set fullheart sprite if the index is less than the lives variable
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart; // set emptyheart sprite if the index is greater than or equal to the lives variable
+            }
+        }
+    }
+}
